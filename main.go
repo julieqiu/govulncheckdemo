dbe13fa9aa1daa47649df5e6cf8d57b9424ea0b4
Julie Qiu <julie@golang.org>
2023-03-21 15:13:57 -0400

    main.go: add script to regenerate modules

--- main.go
+++ main.go
@@ -4,23 +4,73 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"io/fs"
+	"log"
 	"os"
-
-	"github.com/tidwall/gjson"
-	"golang.org/x/text/language"
+	"os/exec"
+	"path/filepath"
+	"strings"
 )
 
+var update = flag.Bool("update", false, "update .txt and .json files for all modules")
+
 func main() {
-	for _, arg := range os.Args[1:] {
-		tag, err := language.Parse(arg)
+	if err := run(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func run() error {
+	return filepath.Walk(".", func(path string, info fs.FileInfo, err error) error {
 		if err != nil {
-			fmt.Printf("%s: error: %v\n", arg, err)
-		} else if tag == language.Und {
-			fmt.Printf("%s: undefined\n", arg)
-		} else {
-			fmt.Printf("%s: tag %s\n", arg, tag)
+			fmt.Printf("prevent panic by handling failure accessing a path %q: %v\n", path, err)
+			return err
+		}
+		if info.IsDir() && strings.HasPrefix(info.Name(), "module") {
+			return runGovulncheck(info.Name())
+		}
+		return nil
+	})
+}
+
+func runGovulncheck(dir string) error {
+	abs, err := filepath.Abs(".")
+	if err != nil {
+		return err
+	}
+	abs = abs + "/" + dir
+	for _, mode := range []struct {
+		filename string
+		args     []string
+	}{
+		{"output.txt", []string{abs}},
+		{"verbose.txt", []string{"-v", abs}},
+		{"output.json", []string{"-json", abs}},
+	} {
+		cmd := exec.Command("govulncheck", mode.args...)
+		cmd.Dir = abs
+		fmt.Printf("Running %q\n", strings.Join(cmd.Args, " "))
+		cmd.Stderr = os.Stderr
+		out, err := cmd.Output()
+		exiterr, _ := err.(*exec.ExitError)
+		if err != nil && exiterr.ExitCode() != 3 {
+			return fmt.Errorf("govulncheck error: %v", err)
+		}
+		if err := writeFile(dir+"/"+"output.json", out); err != nil {
+			return err
 		}
 	}
-	gjson.Valid("{hello: world}")
+	return nil
+}
+
+func writeFile(filename string, output []byte) error {
+	f, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	_, err = f.Write(output)
+	return err
 }
